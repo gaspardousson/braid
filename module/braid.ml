9de4c04ce4822faf8f41aa5caf4a6127dcf57f81
gaspardousson <gsapardousson>
2021-06-14 20:59:18 +0200

    Add the complete code

--- module/braid.ml
+++ module/braid.ml
@@ -1,86 +1,83 @@
-(* Notations *)
-type sigma = int;;
-type word = sigma list;;
+module Braid :
+sig
 
-(* Translation *)
-let string_of_sigma = string_of_int;;
+    val string_of_sigma : int -> string
+    val string_of_braid : int list -> string
+    
+    val inverse : int list -> int list
+    val is_pure: int list -> bool
 
-let string_of_word w =
-    let rec aux w = match w with
+    val rewrite: int list -> int list
+    val decompose: int list -> (int list)*(int list)
+
+    val is_empty: int list -> bool
+    val is_equiv: int list -> int list -> bool
+
+end =
+struct
+
+let string_of_sigma = string_of_int
+let string_of_braid b =
+    let rec aux b = match b with
         [] -> "]"
         |t::[] -> (string_of_sigma t)^"]"
         |t::q -> (string_of_sigma t)^";"^(aux q)
-    in "["^(aux w);;
+    in "["^(aux b)
 
-
-(* Basics of word algewra *)
-let inverse w =
-    let rec aux w acc = match w with
+let inverse b =
+    let rec aux b acc = match b with
         [] -> acc
         |t::q -> aux q (-t::acc)
-    in aux w [];;
+    in aux b []
 
-(* TODO obvious simplifications *)
-let rec simplify (w:word) :word = match w with
-    [] -> []
-    |0::q -> simplify q
-    |t::0::q -> simplify (t::q)
-    |t1::t2::q when t1 = -t2 -> simplify q
-    |t::q -> t::(simplify q);;
-
-let is_simplified (w:word) :bool = (w = simplify w);;
-
-
-
-(* Testing if a word is pure *)
-let max l = match l with
-    [] -> 0
-    |t::q -> let rec aux l m = match l with
-                [] -> m
-                |t::q when t>m -> aux q t
-                |_::q -> aux q m
-             in aux q t;;
-
-let is_pure w =
-    let rec aux top bot n read_w w  = match read_w with
-        [] when bot = top -> if top < n then aux (top+1) (top+1) n w w else true
+let is_pure b =
+    let max b = match b with
+        [] -> 0
+        |t::q -> let rec aux l m = match l with
+                    [] -> m
+                    |t::q when t>m -> aux q t
+                    |_::q -> aux q m
+                 in aux q t
+    in
+    let rec aux top bot n read_w b  = match read_w with
+        [] when bot = top -> if top < n then aux (top+1) (top+1) n b b else true
         |[] -> false
-        |0::q -> aux top bot n q w
-        |t::q -> let x=abs t in if x=bot-1 then aux top (bot-1) n q w
-                                else if x=bot then aux top (bot+1) n q w
-                                else aux top bot n q w
-    in aux 1 1 (max w) w w;;
+        |0::q -> aux top bot n q b
+        |t::q -> let x=abs t in if x=bot-1 then aux top (bot-1) n q b
+                                else if x=bot then aux top (bot+1) n q b
+                                else aux top bot n q b
+    in aux 1 1 (max b) b b
 
-(* Rewriting algorithm *)
-let rewrite w =
-    let rec mirror w acc = match w with
+let rewrite b =
+    let rec mirror b acc = match b with
         [] -> acc
         |t::q -> mirror q (t::acc)
     in
-    let rec aux w1 w2 = match w1 with
-        [] -> w2
-        |0::q1 -> (match w2 with
+    let rec aux b1 b2 = match b1 with
+        [] -> b2
+        |0::q1 -> (match b2 with
             [] -> aux q1 []
             |h::q2 -> aux (h::q1) q2)
-        |i::j::q1 when i<0 && j>0 && abs(i+j)=0 -> (match w2 with
+        |i::j::q1 when i<0 && j>0 && abs(i+j)=0 -> (match b2 with
             [] -> aux q1 []
             |h::q2 -> aux (h::q1) q2)
-        |i::j::q1 when i<0 && j>0 && abs(i+j)=1 -> (match w2 with
+        |i::j::q1 when i<0 && j>0 && abs(i+j)=1 -> (match b2 with
             [] -> aux (j::(-i)::(-j)::i::q1) []
             |h::q2 -> aux (h::j::(-i)::(-j)::i::q1) q2)
-        |i::j::q1 when i<0 && j>0 && abs(i+j)>1 -> (match w2 with
+        |i::j::q1 when i<0 && j>0 && abs(i+j)>1 -> (match b2 with
             [] -> aux (j::i::q1) []
             |h::q2 -> aux (h::j::i::q1) q2)
-        |h::q -> aux q (h::w2)
-    in mirror (aux w []) [];;
+        |h::q -> aux q (h::b2)
+    in mirror (aux b []) []
 
-let break_down w =
-    let rec aux w = match w with
-        |t::q when t>0 -> let u,v = aux q in t::u,v
-        |v -> [],v
-    in let u,v = aux w in u,inverse v;;
+let decompose b =
+    let rec aux b = match b with
+        |h::q when h>0 -> let b1,b2 = aux q in h::b1,b2
+        |b2 -> [],b2
+    in let b1,b2 = aux b in b1,inverse b2
 
-let is_empty w =
-    let u,v = break_down (rewrite w) in rewrite ((inverse u)@v) = [];;
 
-let is_equiv w1 w2 = is_empty (w1@(inverse w2));;
+let is_empty b = let b1,b2 = decompose (rewrite b) in rewrite ((inverse b1)@b2) = [];;
+let is_equiv b1 b2 = is_empty (b1@(inverse b2))
+
+end;;
